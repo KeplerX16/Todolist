21f905d61a9ef59bae0fe5ade3fe4d48bd73a107
KeplerX16 <necromancerx.16@gmail.com>
2025-06-04 11:15:23 +0530

    First Commit:Todolist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base:"/Todolist/",
+  base: '/Todolist/',
   plugins: [react(),tailwindcss()],
 })
